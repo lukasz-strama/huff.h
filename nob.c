54455caa18784b5a911204af22aa94353246e475
Łukasz Strama <strama.lukasz54@gmail.com>
2025-11-21 17:06:03 +0100

    Refactor build process and add test runner implementation

--- nob.c
+++ nob.c
@@ -16,7 +16,14 @@ int main(int argc, char **argv)
     nob_cc_output(&cmd, BUILD_FOLDER "huff");
     nob_cc_inputs(&cmd, "main.c");
     nob_cmd_append(&cmd, "-lm");
-    if (!nob_cmd_run(&cmd)) return 1;
+    if (!nob_cmd_run_sync(cmd)) return 1;
+
+    // Build test runner
+    cmd.count = 0;
+    nob_cmd_append(&cmd, "cc");
+    nob_cmd_append(&cmd, "-Wall", "-Wextra", "-O3", "-march=native", "-pthread");
+    nob_cmd_append(&cmd, "-o", "build/test", "test.c", "-lm");
+    if (!nob_cmd_run_sync(cmd)) return 1;
 
     return 0;
 }
